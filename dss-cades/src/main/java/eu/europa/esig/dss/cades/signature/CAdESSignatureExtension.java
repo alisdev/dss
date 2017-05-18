91dd07e7917db57d0377b99d4a1e741053b680f8
alisdev <coufal@alis.cz>
2017-05-18 15:37:52 +0200

    SSLJDR-1227

--- dss-cades/src/main/java/eu/europa/esig/dss/cades/signature/CAdESSignatureExtension.java
+++ dss-cades/src/main/java/eu/europa/esig/dss/cades/signature/CAdESSignatureExtension.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Hex;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
